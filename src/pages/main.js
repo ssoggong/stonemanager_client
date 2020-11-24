d786a5e338531bf88e2c4d8bd2fbb6d36d55048a
Choiyunh_ <svs101@naver.com>
2020-11-25 02:31:07 +0900

    update main

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import {Nav, Navbar, Form, Card, Button} from 'react-bootstrap'
 
 class main extends Component {
 
   render() {
     return (
-        <div>
-            <h3> This is main </h3>
+        <div className="main">
+            <Navbar bg="primary" variant="dark">
+                <Navbar.Brand href="#home"><b>Stone Manager</b></Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">내 정보</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <Button variant="outline-light">회원가입</Button>
+                    &nbsp;&nbsp;
+                    <Button variant="outline-light">로그인</Button>
+                </Form>
+            </Navbar>
+
+            <div className="projectList">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Title>Project 1</Card.Title>
+                            <Card.Text>
+                            abc
+                            </Card.Text>
+                            <Button variant="primary">Go</Button>
+                        </Card.Body>
+                    </Card>
+
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Title>Project 2</Card.Title>
+                            <Card.Text>
+                            abc
+                            </Card.Text>
+                            <Button variant="primary">Go</Button>
+                        </Card.Body>
+                    </Card>
+            </div>
+            
+            
+
+            
         </div>
     );
   }
--- src/pages/myPage.js
+++ src/pages/myPage.js
@@ -6,25 +6,22 @@ import userSolid from './user-solid.svg';
 class myPage extends Component {
   render() {
     return (
-      <div className='myPage'>
+      <div className="myPage">
         <div className="blueNav">
-          <div className="projectName"> 
-            <li><b><a href="">Stone Manager</a></b></li>
-            <li><b><a href="">내 정보</a></b></li>
+          <div className="projectName">
+            <li><b>Stone Manager</b></li>
+            <li><b>내 정보</b></li>
           </div>
+
           <div className="info">
-            <a href=""><img src = {userSolid} className="userImg" /></a>
-            <li><b><a href="">마이 페이지</a></b></li>
-            <li><b><a href="">로그아웃</a></b></li>
-          </div>          
+            <li><img src = {userSolid} className="userImg" /></li>
+            <li><b>마이 페이지</b></li>
+            <li><b>로그아웃</b></li>
+          </div>
         </div>
-
-        <div className="myProfile">
-          <hr></hr>
-          <h4>test</h4>
-          <button className="btn btn-primary">Click</button>
+        <div>
+          ddd
         </div>
-
       </div>
       
     );
