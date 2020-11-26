0fc96bae24e8e9c14fffa8b517f5786c724558f0
Choiyunh_ <svs101@naver.com>
2020-11-26 18:14:18 +0900

    test schedule

--- src/pages/homeContent.js
+++ src/pages/homeContent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ProgressBar, Container, Row, Col } from 'react-bootstrap';
+import { ProgressBar, Container, Row, Col, Card, Button } from 'react-bootstrap';
 import '../App.css';
 
 function homeContent() {
@@ -8,7 +8,7 @@ function homeContent() {
     return (
         <div className='homeContent'>
             <Container>
-                <Row style={{marginTop:200, marginLeft:100}}>
+                <Row style={{marginTop:300, marginLeft:100}}>
                     <Col sm> 
                         <h1>진행률</h1> 
                     </Col>
@@ -19,6 +19,25 @@ function homeContent() {
                 <Row style={{marginTop:150, marginLeft:100}}>
                     <Col sm> <h1> 참여율 </h1> </Col>
                     <Col sm> <h1> 디데이 </h1> </Col>
+                </Row>
+                <Row style={{marginTop:25, marginLeft:100}}>
+                    <Col sm={6}> <h1> Graph </h1> </Col>
+                    <Col sm={3}>
+                        <Card style={{ width: '15rem', display: 'flex' }}>
+                            <Card.Body>
+                                <Card.Title>코딩 완료</Card.Title>                               
+                                <Button variant="danger">D-1</Button>                                  
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={3}>
+                        <Card style={{ width: '15rem', display: 'flex' }}>
+                            <Card.Body>
+                                <Card.Title>정기 미팅</Card.Title>                               
+                                <Button variant="warning">D-2</Button>                                  
+                            </Card.Body>
+                        </Card>
+                    </Col>                    
                 </Row>
             </Container>
             
