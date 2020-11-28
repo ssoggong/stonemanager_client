00e6569d9035d78c31624475cefb9f70ff452e0f
YoungHoon Kim <yhkim5688@naver.com>
2020-11-29 02:47:10 +0900

    axios 설치 및 axios.get 통신 예제

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Nav, Navbar, Form, Button, Container, Row, Col } from 'react-bootstrap'
 import {
     Route,
@@ -10,53 +10,66 @@ import { Register } from "./register";
 import { FindPW } from "./findPassword";
 import { CreateProject } from "./createProject";
 import ProjectList from './ProjectList';
+import axios from 'axios'
 
-function Main() {
-    return (
-        <div className="main">
-            <Navbar bg="primary" variant="dark">
-                <Link to='#'>
-                    <Navbar.Brand>
-                        <b>Stone Manager</b>
-                    </Navbar.Brand>
-                </Link>
-                <Nav className="mr-auto">
-
-                </Nav>
-                <Form inline>
-                    <Link to="/register">
-                        <Button variant="outline-light">회원가입</Button>
+export function getSubject() {
+    debugger;
+    axios.get('http://ec2-13-125-249-225.ap-northeast-2.compute.amazonaws.com:8080/users/subject',{
+        headers: {
+            "Content-Type": "application/json",
+            "userIndex": 1
+        }
+    })
+}
+class Main extends Component {
+    componentDidMount() {
+        getSubject()
+    }
+    render() {
+        return (
+            <div className="main">
+                <Navbar bg="primary" variant="dark">
+                    <Link to='#'>
+                        <Navbar.Brand>
+                            <b>Stone Manager</b>
+                        </Navbar.Brand>
                     </Link>
-                    &nbsp;&nbsp;
-                        <Link to="/login">
-                        <Button variant="outline-light">로그인</Button>
-                    </Link>
-                </Form>
-            </Navbar>
+                    <Nav className="mr-auto">
 
-            <Container>            
-                <ProjectList />
-                <Row style={{ marginLeft: 470 }}>
-                    <Col sm>
-                        <Link to="/createProject">
-                            <Button variant="primary">New Proejct</Button>
+                    </Nav>
+                    <Form inline>
+                        <Link to="/register">
+                            <Button variant="outline-light">회원가입</Button>
                         </Link>
-                    </Col>
-                </Row>
-            </Container>
+                        &nbsp;&nbsp;
+                        <Link to="/login">
+                            <Button variant="outline-light">로그인</Button>
+                        </Link>
+                    </Form>
+                </Navbar>
+
+                <Container>
+                    <ProjectList/>
+                    <Row style={{marginLeft: 470}}>
+                        <Col sm>
+                            <Link to="/createProject">
+                                <Button variant="primary">New Proejct</Button>
+                            </Link>
+                        </Col>
+                    </Row>
+                </Container>
 
 
+                <BrowserRouter>
+                    <Route path="/createProject" component={CreateProject}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/register" component={Register}/>
+                    <Route path="/findPassword" component={FindPW}/>
+                </BrowserRouter>
 
-            <BrowserRouter>
-                <Route path="/createProject" component={CreateProject} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-                <Route path="/findPassword" component={FindPW} />
-            </BrowserRouter>
-
-        </div>
-    );
-
+            </div>
+        );
+    }
 }
 
 export default Main;
